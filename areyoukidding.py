6b726ef3cb9c380b3e60731b692cf7ec652d2d7b
syo <hadlec@rpi.edu>
2016-11-12 21:22:35 -0500

    workaround for the throttling

--- areyoukidding.py
+++ areyoukidding.py
@@ -1,17 +1,30 @@
 import json
 from sodapy import Socrata
 
+dataset_id = 'qbxx-ev3s'
+page_offset = 0
+results = []
+
+################################
+
+
 #client = Socrata(api_endpoint, app_token)
 client = Socrata('data.cityofboston.gov', None)
-
-dataset_id = 'qbxx-ev3s'
 
 street = raw_input('Enter the street you received the ticket on: ').upper()
 
 print "RECEIVED INPUT '" + street + "'"
 
 print "QUERYING FOR STREET"
-test = client.get(dataset_id, street_nam=street)
+
+tmp = ""
+
+while (len(tmp) >= 1000 or page_offset == 0):
+    tmp = client.get(dataset_id, q=street, offset=page_offset)
+    results.append(tmp)
+    page_offset += 1000
+
+
 print "STREET QUERIED"
 #jsonelem[elem_name][row_of_json][index_of_list]
 #print test[0]
@@ -23,8 +36,11 @@ print "STREET QUERIED"
 
 print "\n"
 
-for row in test:
-    print row['ticket_loc'] + " with a violation of: " + row['violation1']
-print str(len(test)) + " Results Found"
+result_count = 0
+for page in results:
+    result_count += len(page)
+    for row in page:
+        print row['ticket_loc'] + " with a violation of: " + row['violation1']
+print str(result_count) + " Results Found"
 
 #print json.dumps(test)
