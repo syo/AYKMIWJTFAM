e9782803333173954a682d8a78979bd3910d38b6
syo <hadlec@rpi.edu>
2016-11-12 20:44:14 -0500

    lookup fixed, reasons given

--- areyoukidding.py
+++ areyoukidding.py
@@ -1,3 +1,4 @@
+import json
 from sodapy import Socrata
 
 #client = Socrata(api_endpoint, app_token)
@@ -5,6 +6,25 @@ client = Socrata('data.cityofboston.gov', None)
 
 dataset_id = 'qbxx-ev3s'
 
-test = client.get(dataset_id, limit=2)
+street = raw_input('Enter the street you received the ticket on: ').upper()
 
-print test
+print "RECEIVED INPUT '" + street + "'"
+
+print "QUERYING FOR STREET"
+test = client.get(dataset_id, street_nam=street)
+print "STREET QUERIED"
+#jsonelem[elem_name][row_of_json][index_of_list]
+#print test[0]
+
+#test1 = json.dumps(test[0])
+
+#print "\n\n\n"
+#print test1
+
+print "\n"
+
+for row in test:
+    print row['ticket_loc'] + " with a violation of: " + row['violation1']
+print str(len(test)) + " Results Found"
+
+#print json.dumps(test)
