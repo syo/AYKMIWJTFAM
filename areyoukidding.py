4ad34a2539934abe88b7e479b0da20248997207a
JoeIaquinto <iaquinto.joe@gmail.com>
2016-11-14 18:06:25 -0500

    Convert to python 3.5, add Folium for static map output of markers for query results where a geolocation was found.

--- areyoukidding.py
+++ areyoukidding.py
@@ -1,5 +1,7 @@
 import json
+import sys
 from sodapy import Socrata
+import folium
 #from tkinter import *
 
 dataset_id = 'qbxx-ev3s'
@@ -14,11 +16,31 @@ def reslen():
         ct += len(page)
     return str(ct)
 
+def printAll(row):
+    for key in row.keys():
+        print("Key: " + key + " Value: " + row[key])
+
 def printResults():
+    i = 1
+    markers=[]
     for page in results:
         for row in page:
-            print row['ticket_loc'] + " with a violation of: " + row['violation1']
-    print reslen() + " Results Found"
+            to_print = "{}  {}\n".format(row['issue_time'],str(row['issue_date']))
+            to_print += "Violation {}: {} Fine: ${}".format(row['violation_'],row['violation1'],row['fine_amt'])
+            print("Ticket Number: {}  Location: {}".format(i,row['ticket_loc']))
+            print(to_print)
+            if 'lat' in row.keys() and 'long' in row.keys():
+                print("Geolocation found: ({} , {})".format(row['lat'],row['long']))
+                markers.append(([row['lat'],row['long']], to_print))
+            i += 1
+    makeMap(markers)
+    print(reslen() + " Results Found")
+
+def makeMap(markers):
+    map_1 = folium.Map(location=[42.350489, -71.069099], zoom_start=10)
+    for marker in markers:
+        folium.Marker(location=marker[0], popup=marker[1]).add_to(map_1)
+    map_1.save('tickets.html')
 
 def queryStreet(street):
     client = Socrata('data.cityofboston.gov', None)
@@ -29,7 +51,6 @@ def queryStreet(street):
         results.append(tmp)
         page_offset += 1000
 
-
 if __name__ == '__main__':
-   queryStreet(raw_input('Enter the street you received the ticket on: ').upper())
+   queryStreet(input('Enter the street you received the ticket on: ').upper())
    printResults()
